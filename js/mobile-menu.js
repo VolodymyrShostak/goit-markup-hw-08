5cba6e328de4065a35d29aa99c766d0e14488632
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-09-13 17:50:04 +0300

    робота над хедером мобільним

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,23 +1,29 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+;(() => {
+    const mobileMenu = document.querySelector('.js-menu-container')
+    const openMenuBtn = document.querySelector('.js-open-menu')
+    const closeMenuBtn = document.querySelector('.js-close-menu')
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
- 
-Add heading textAdd bold text, <Ctrl+b>Add italic text, <Ctrl+i>
+    const toggleMenu = () => {
+        const isMenuOpen =
+            openMenuBtn.getAttribute('aria-expanded') === 'true' || false
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
+        mobileMenu.classList.toggle('is-open')
+    }
+
+    openMenuBtn.addEventListener('click', () => {
+        toggleMenu()
+        document.body.style.overflow = 'hidden'
+    })
+    closeMenuBtn.addEventListener('click', () => {
+        toggleMenu()
+        document.body.style.overflow = ''
+    })
+
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
+        if (!e.matches) return
+        mobileMenu.classList.remove('is-open')
+        document.body.style.overflow = ''
+        openMenuBtn.setAttribute('aria-expanded', false)
+    })
+})()
